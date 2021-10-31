d245de848cb37b83b2a4d0503db0448da4ab3d8f
Peter Yates <peter.yates@graphia.co.uk>
2021-10-31 09:48:56 +0000

    Stop referring to instance variables in helpers

--- app/helpers/wizardry_helper.rb
+++ app/helpers/wizardry_helper.rb
@@ -1,25 +1,25 @@
 module WizardryHelper
-  def wizardry_content
-    case @wizard.current_page
+  def wizardry_content(w)
+    case w.current_page
     when Wizardry::Pages::QuestionPage
-      render_form
+      render_form(w)
     when Wizardry::Pages::CheckYourAnswersPage
-      safe_join([render_check_your_answers, render_form])
+      safe_join([render_check_your_answers(w), render_form(w)])
     when Wizardry::Pages::CompletionPage
-      render_completion
+      render_completion(w)
     end
   end
 
 private
 
-  def render_form
+  def render_form(w)
     capture do
-      try_partial('page') do
-        wizard_form do |f|
-          try_partial('form', locals: { f: f }) do
+      try_partial(w, 'page') do
+        wizard_form(w) do |f|
+          try_partial(w, 'form', locals: { f: f }) do
             concat(f.govuk_error_summary)
 
-            @wizard.current_page.questions.map do |q|
+            w.current_page.questions.map do |q|
               concat(f.send(q.form_method, q.name, *q.extra_args, **q.extra_kwargs))
             end
 
@@ -30,18 +30,18 @@ private
     end
   end
 
-  def render_check_your_answers
+  def render_check_your_answers(w)
     capture do
-      try_partial('page') do
+      try_partial(w, 'page') do
         safe_join(
           [
             render(GovukComponent::SummaryListComponent.new) do |summary_list|
-              @wizard.route.each do |page|
+              w.route.each do |page|
                 page.questions.each do |question|
                   summary_list.row do |sl|
-                    sl.key(text: check_your_answers_key(@wizard.object.class.name, question.name))
-                    sl.value(text: @wizard.object.send(question.name))
-                    sl.action(href: send(@wizard.framework.edit_path_helper, page.name))
+                    sl.key(text: check_your_answers_key(w.object.class.name, question.name))
+                    sl.value(text: w.object.send(question.name))
+                    sl.action(href: send(w.framework.edit_path_helper, page.name))
                   end
                 end
               end
@@ -52,9 +52,9 @@ private
     end
   end
 
-  def render_completion
+  def render_completion(w)
     capture do
-      try_partial('page') do
+      try_partial(w, 'page') do
         safe_join(
           [
             tag.h1('Completed'),
@@ -72,15 +72,15 @@ private
     end
   end
 
-  def wizard_form(turbo_frame_id: 'wizardry-form', &block)
+  def wizard_form(w, turbo_frame_id: 'wizardry-form', &block)
     turbo_frame_tag(turbo_frame_id) do
       safe_join(
         [
           form_for(
-            @wizard.object,
+            w.object,
             url: send(
-              @wizard.framework.update_path_helper,
-              page: @wizard.current_page.name
+              w.framework.update_path_helper,
+              page: w.current_page.name
             ),
             method: :patch,
             builder: GOVUKDesignSystemFormBuilder::FormBuilder,
@@ -91,11 +91,11 @@ private
     end
   end
 
-  def try_partial(section, locals: {}, &block)
-    if lookup_context.template_exists?(partial_file_path(section))
-      Rails.logger.debug("🧙 Page partial #{partial_file_path(section)} found; rendering entire page")
+  def try_partial(w, section, locals: {}, &block)
+    if lookup_context.template_exists?(partial_file_path(w, section))
+      Rails.logger.debug("🧙 Page partial #{partial_file_path(w, section)} found; rendering entire page")
 
-      concat(render(partial: partial_render_path(section), locals: locals))
+      concat(render(partial: partial_render_path(w, section), locals: locals))
     else
       Rails.logger.debug('🧙 No overriding form partial found; automatically building form')
 
@@ -109,12 +109,12 @@ private
     I18n.t("helpers.label.#{class_name.underscore}.#{question_name}")
   end
 
-  def partial_file_path(suffix)
-    "#{@wizard.framework.name}/_#{@wizard.current_page.name}_#{suffix}"
+  def partial_file_path(w, suffix)
+    "#{w.framework.name}/_#{w.current_page.name}_#{suffix}"
   end
 
-  def partial_render_path(suffix)
-    "#{@wizard.framework.name}/#{@wizard.current_page.name}_#{suffix}"
+  def partial_render_path(w, suffix)
+    "#{w.framework.name}/#{w.current_page.name}_#{suffix}"
   end
 end
 
